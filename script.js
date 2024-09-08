7ea541d837120d77442670b61632f54d5958e537
Subhadipjana2004 <151117062+Subhadipjana2004@users.noreply.github.com>
2024-09-08 08:14:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text",{
+    strings:["Frontend Designer","web Developer","Content Creator "],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true,
+})
